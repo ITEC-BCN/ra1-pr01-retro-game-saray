2c5abc2ef41ac7f5dae7a421c1fcd15916f2e841
Saray Varela <torresyoselyn06@gmail.com>
2025-12-02 19:06:43 +0100

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+function FadeToWhite (Time: number) {
+    color.startFade(color.originalPalette, color.White, Time / 2)
+    color.pauseUntilFadeDone()
+    color.startFade(color.White, color.originalPalette, Time / 2)
+}
+function SpawnText (text: string, X: number, Y: number) {
+    textSprite = textsprite.create(text, 0, 5)
+    textSprite.setPosition(X, Y)
+    textSprite.setOutline(1, 14)
+}
+let textSprite: TextSprite = null
+let game2 = false
+let menu = false
+let start = true
+FadeToWhite(4000)
+scene.setBackgroundImage(assets.image`start background`)
+SpawnText("Press Start", 76, 110)
